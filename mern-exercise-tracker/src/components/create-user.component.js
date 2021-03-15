8cb0b8d4ede6fca1d11e12a6a3cf68be68c1e93f
can <21026424+nzmcno@users.noreply.github.com>
2021-03-15 05:15:04 +0000

    my first app finished with 1 bug!

--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import axios from 'axios';
 export default class CreateUser extends Component {
     constructor(props) {
         super(props);
@@ -27,8 +27,8 @@ export default class CreateUser extends Component {
     
         console.log(user);
     
-        //axios.post('http://localhost:5000/users/add', user)
-         // .then(res => console.log(res.data));
+        axios.post('http://localhost:5000/users/add', user)
+         .then(res => console.log(res.data));
     
         this.setState({
           username: ''
